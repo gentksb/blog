8e3d53155c4740071d7164e4a1c5055fe1ba8991
gensobunya <osa.gensobunya@gmail.com>
2019-09-21 20:28:20 +0900

    postlist にstyled component

--- src/components/postcardlist.js
+++ src/components/postcardlist.js
@@ -4,46 +4,56 @@ import { rhythm } from "../utils/typography"
 import Thumbnail from "../components/thumbnail"
 import styled from "styled-components"
 
-const ThumbnailBox = styled.div`
-`
-class PostCardList extends React.Component {
-  render(){
-    const {nodes} = this.props
-  
+const PostCardList = ({ nodes }) => {
+  return nodes.map(({ node }) => {
+    const title = node.frontmatter.title || node.fields.slug
+
     return (
-      nodes.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.excerpt,
-                }}
-              />
-            </section>
-            <ThumbnailBox>
-              <Thumbnail filename={node.frontmatter.featuredImage.base} alt=""/>
-            </ThumbnailBox>
-          </article>
-          )
-        }
-      )
+      <ArticleContainer>
+        <article key={node.fields.slug}>
+          <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+            <HeaderBox>
+              <PostTitle>{title}</PostTitle>
+              <DateBox>
+                <small>{node.frontmatter.date}</small>
+              </DateBox>
+              <ThumbnailWrapper>
+                <Thumbnail
+                  filename={node.frontmatter.featuredImage.base}
+                  alt=""
+                />
+              </ThumbnailWrapper>
+            </HeaderBox>
+            <SectionWrapper>
+              <section>
+                <PostDescription
+                  dangerouslySetInnerHTML={{
+                    __html: node.excerpt,
+                  }}
+                />
+                (続きを読む)
+              </section>
+            </SectionWrapper>
+          </Link>
+        </article>
+      </ArticleContainer>
     )
-  }
+  })
 }
 
+const ArticleContainer = styled.div``
+const HeaderBox = styled.header``
+const PostTitle = styled.h3`
+  marginbottom: ${rhythm(1 / 4)};
+`
+const DateBox = styled.div`
+  margin: 0;
+  text-align: right;
+`
+const ThumbnailWrapper = styled.div``
+const SectionWrapper = styled.div``
+const PostDescription = styled.p`
+  text-decoration: none;
+  color: #000000;
+`
 export default PostCardList
