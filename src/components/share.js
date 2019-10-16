1962354a7304f68b93338028c812bd093020cc08
gen <gen@gensobunya.net>
2019-10-16 10:31:42 +0900

    add share button area

---
+++ src/components/share.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const sharebox = (props) => {
+  const { postdata } = props
+
+  return (
+    <div>
+    Make share button to Twitter/Facebook/General share
+    </div>
+  )
+}
+
+export default Share
