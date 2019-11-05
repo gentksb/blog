a7e33998311dabd7e11233c02d56566758d21122
Gen.T. (gensobunya) <36843875+gentksb@users.noreply.github.com>
2019-11-05 10:30:48 +0900

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,23 +71,11 @@ function SEO({ description, lang, meta, title, image, location }) {
         },
         {
           name: `twitter:card`,
-          content: `summary_large_image`,
+          content: `summary`,
         },
         {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-        {
-          name: `twitter:image`,
-          content: metaImage,
+          name: `twitter:site`,
+          content: site.siteMetadata.social.twitter,
         }
       ].concat(meta)}
     />
