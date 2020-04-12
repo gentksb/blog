d13cf3481a427b29ec428bf40ee42c88fd2b3e5a
gensobunya <gen@gensobunya.net>
2020-04-12 14:47:19 +0900

    pagination類のUIボタン化

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import React from "react"
-import { Card, Grid, CardActionArea, CardContent, Typography } from '@material-ui/core'
-import { ArrowBackIos, ArrowForwardIos } from '@material-ui/icons'
+import { Grid, Button } from '@material-ui/core'
+import { ArrowBack, ArrowForward } from '@material-ui/icons'
 import styled from "@emotion/styled";
 
 const Pagination = ({ props }) => {
@@ -15,36 +15,26 @@ const Pagination = ({ props }) => {
   return (
     <Grid container spacing={2} justify="center">
       <Grid item xs={6}>
-        <Card style={{ height: "100%" }}>
-          <CardActionArea style={{ height: "100%" }}>
-            {nextPagePath ?
-              <PaginationLink to={nextPagePath}>
-                <CardContent>
-                  <Grid container>
-                    <Grid item xs={2}><ArrowBackIos /></Grid>
-                    <Grid item xs={10}><Typography variant="button">Older</Typography></Grid>
-                  </Grid>
-                </CardContent>
-              </PaginationLink>
-              : null}
-          </CardActionArea>
-        </Card>
+        {nextPagePath ?
+          <PaginationLink to={nextPagePath}>
+            <Button variant="outlined" color="primary" startIcon={<ArrowBack />}>
+              Next Page
+            </Button>
+          </PaginationLink>
+          : <Button variant="outlined" disabled startIcon={<ArrowBack />}>
+            Older Posts
+            </Button>}
       </Grid>
       <Grid item xs={6} style={{ textAlign: "right" }}>
-        <Card style={{ height: "100%" }}>
-          <CardActionArea style={{ height: "100%" }}>
-            {previousPagePath ?
-              <PaginationLink to={previousPagePath}>
-                <CardContent>
-                  <Grid container>
-                    <Grid item xs={10}><Typography variant="button">Newer</Typography></Grid>
-                    <Grid item xs={2}><ArrowForwardIos /></Grid>
-                  </Grid>
-                </CardContent>
-              </PaginationLink>
-              : null}
-          </CardActionArea>
-        </Card>
+        {previousPagePath ?
+          <PaginationLink to={previousPagePath}>
+            <Button variant="outlined" color="primary" endIcon={<ArrowForward />}>
+              Newer Posts
+            </Button>
+          </PaginationLink>
+          : <Button variant="outlined" disabled endIcon={<ArrowForward />}>
+            Newer Posts
+            </Button>}
       </Grid>
     </Grid>
   )
