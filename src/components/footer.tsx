d63756dbbc6724705ca5650123a6d7922e3139ae
gensobunya <gen@gensobunya.net>
2020-12-18 01:05:26 +0900

    カウントできるように変更

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,8 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { FooterComponentQuery } from "../../types/graphql-types"
 import { AppBar, Typography } from "@material-ui/core"
 import Bio from "./bio"
 
-const Footer: React.FunctionComponent = () => {
-  const data: FooterComponentQuery = useStaticQuery(graphql`
-    query FooterComponent {
-      blogmura: file(absolutePath: { regex: "/blogmura.gif/" }) {
-        publicURL
-      }
-    }
-  `)
-  
-  return (
+const Footer: React.FunctionComponent = () => (
   <AppBar position="static" component="footer">
     <Bio />
     <Typography align="center" variant="caption">
@@ -24,13 +13,10 @@ const Footer: React.FunctionComponent = () => {
         website.
       </p>
       <a href="https://blogmura.com/profiles/11085449?p_cid=11085449">
-        <img
-          src={data.blogmura.publicURL}
-          alt="PVアクセスランキング にほんブログ村"
-        />
+        <img src="https://blogparts.blogmura.com/parts_image/user/pv11085449.gif" alt="PVアクセスランキング にほんブログ村" />
       </a>
     </Typography>
   </AppBar>
-)}
+)
 
 export default Footer
