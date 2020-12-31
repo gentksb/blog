26bec81a216156970110437fd4fe41f32dfbec28
gensobunya <gen@gensobunya.net>
2020-12-31 18:58:38 +0900

    postTag.tsxをChakraUIに

--- src/components/molecules/postTag.tsx
+++ src/components/molecules/postTag.tsx
@@ -1,17 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Chip, Container } from "@material-ui/core"
-import { LocalOffer } from '@material-ui/icons';
-import styled from "@emotion/styled"
-
-const TagChip = styled(Chip)`
-  margin: auto auto auto 8px;
-`
-const TagContainer = styled(Container)`
-  padding:0;
-  margin: 8px auto;
-`
-
+import { HStack, Tag } from "@chakra-ui/react"
 interface Props {
   tags?: string[]
 }
@@ -22,17 +11,21 @@ const PostTag: React.FunctionComponent<Props> = ({ tags }) => {
     (
       tags.map((tag) => (
         <Link to={`/tags/${tag.toLowerCase()}`} style={{ textDecoration: 'none' }} key={tag}>
-          <TagChip key={tag} label={tag} color="primary" size="small" icon={<LocalOffer />} clickable style={{ textDecoration: `none` }} />
+          <Tag key={tag} size="sm" variant="solid">
+              {tag}
+          </Tag>
         </Link>
       ))
     ) : (
-      <Chip label="No tags" size="small" icon={<LocalOffer />} />
+      <Tag size="sm" variant="solid">
+        No tags
+      </Tag>
     )
 
   return (
-    <TagContainer>
+    <HStack spacing={2}>
       {tagArray}
-    </TagContainer>
+    </HStack>
   )
 }
 
