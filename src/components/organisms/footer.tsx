894534e7095665a3d34b72a1bccc4bca864fef80
gensobunya <gen@gensobunya.net>
2020-12-31 23:03:42 +0900

    footerをchakraに

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -1,22 +1,24 @@
 import React from "react"
-import { AppBar, Typography } from "@material-ui/core"
 import Bio from "../molecules/bio"
+import { Box, Image, Text, VStack } from "@chakra-ui/react"
 
 const Footer: React.FunctionComponent = () => (
-  <AppBar position="static" component="footer">
-    <Bio />
-    <Typography align="center" variant="caption">
-      © {new Date().getFullYear()}, Built with{` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
-      <p>
-        This website uses Cookie to ensure you get the best experience on this
-        website.
-      </p>
-      <a href="https://blogmura.com/profiles/11085449?p_cid=11085449">
-        <img src="https://blogparts.blogmura.com/parts_image/user/pv11085449.gif" alt="PVアクセスランキング にほんブログ村" />
-      </a>
-    </Typography>
-  </AppBar>
+  <footer>
+    <Box position="static" bg="blue.200">
+      <VStack textAlign="center" justifyContent="center">
+        <Bio />
+        <Text fontSize="xs">
+          © {new Date().getFullYear()}, Built with{` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <p>This website uses Cookie to ensure you get the best experience on this
+            website.</p>
+        </Text>
+        <a href="https://blogmura.com/profiles/11085449?p_cid=11085449">
+          <Image src="https://blogparts.blogmura.com/parts_image/user/pv11085449.gif" alt="PVアクセスランキング にほんブログ村" />
+        </a>
+      </VStack>
+    </Box>
+  </footer>
 )
 
 export default Footer
