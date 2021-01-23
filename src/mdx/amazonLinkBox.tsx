fd86018c5d4895a71c08cae0d7e4bd26cacaab3f
gensobunya <gen@gensobunya.net>
2021-01-23 21:53:17 +0900

    LinkBoxレイアウト完了

---
+++ src/mdx/amazonLinkBox.tsx
@@ -0,0 +1,48 @@
+import { Box, Heading, Image, Text } from "@chakra-ui/react"
+import React, { useEffect, useState } from "react"
+
+interface Props {
+  amazon_asin : string
+}
+
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+  const headers = { 'x-requested-with': '' }
+
+const AmazonLinkBox: React.FunctionComponent<Props> = ( {url} ) => {
+  const [ogpData, changeOgpData] = useState(Object)
+  useEffect(() => {
+    try {
+      fetch( proxyUrl + url, {headers: headers})
+        .then(res => res.text())
+        .then(data => {
+          console.log(data)
+          const document = new DOMParser().parseFromString(data, "text/html")
+          const title = document.querySelector("meta[property='og:title']")?.getAttribute('content') || document.querySelector("meta[name='title']")?.getAttribute('content') || document.querySelector('title')?.children.toString() || ""
+          const imageUrl = document.querySelector("meta[property='og:image']")?.getAttribute('content') || ""
+          const description = document.querySelector("meta[property='og:description']")?.getAttribute('content') || document.querySelector("meta[property='description']")?.getAttribute('content') || ""
+          changeOgpData(
+            {
+              title: title,
+              imageUrl: imageUrl,
+              description: description
+            }
+          )
+        })
+    } catch (error) {
+      console.error(error)
+    }
+  },[])
+
+
+  return (
+      <Box>
+        <Image src={ogpData.imageUrl} alt={ogpData.title} />
+        <Box>
+          <Heading>{ogpData.title}</Heading>
+          <Text>{ogpData.description}</Text>
+        </Box>
+      </Box>
+  )
+}
+
+export default AmazonLinkBox
--- src/mdx/linkBox.tsx
+++ src/mdx/linkBox.tsx
@@ -1,4 +1,5 @@
-import { Box, Heading, Image, Text } from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { Box, Button, Image, Link, Text, VStack, SimpleGrid, Flex, Spacer } from "@chakra-ui/react"
 import React, { useEffect, useState } from "react"
 
 interface Props {
@@ -7,9 +8,16 @@ interface Props {
 
   const proxyUrl = "https://cors-anywhere.herokuapp.com/"
   const headers = { 'x-requested-with': '' }
+  const grabberApiEndPoint = "http://favicongrabber.com/api/grab/"
+
+
 
 const LinkBox: React.FunctionComponent<Props> = ( {url} ) => {
   const [ogpData, changeOgpData] = useState(Object)
+  const [faviconData, changeFaviconData] =useState(Object)
+  const urlConstructor = new URL(url)
+  const urlDomain = urlConstructor.hostname
+
   useEffect(() => {
     try {
       fetch( proxyUrl + url, {headers: headers})
@@ -17,16 +25,23 @@ const LinkBox: React.FunctionComponent<Props> = ( {url} ) => {
         .then(data => {
           console.log(data)
           const document = new DOMParser().parseFromString(data, "text/html")
-          const title = document.querySelector("meta[property='og:title']")?.getAttribute('content') || document.querySelector("meta[name='title']")?.getAttribute('content') || document.querySelector('title')?.children.toString() || ""
+          const title = document.querySelector("meta[property='og:title']")?.getAttribute('content') || document.querySelector("meta[name='title']")?.getAttribute('content') || document.querySelector('title')?.innerText || ""
           const imageUrl = document.querySelector("meta[property='og:image']")?.getAttribute('content') || ""
-          const description = document.querySelector("meta[property='og:description']")?.getAttribute('content') || document.querySelector("meta[property='description']")?.getAttribute('content') || ""
+          const description = document.querySelector("meta[property='og:description']")?.getAttribute('content') || document.querySelector("meta[name='description']")?.getAttribute('content') || ""
+          const siteName = document.querySelector("meta[property='og:site_name']")?.getAttribute('content') || urlDomain
+          console.log(title, imageUrl, description)
           changeOgpData(
             {
               title: title,
               imageUrl: imageUrl,
-              description: description
+              description: description,
+              siteName: siteName
             }
           )
+        })
+      fetch(grabberApiEndPoint+urlDomain)
+        .then(res => {
+          changeFaviconData(res)
         })
     } catch (error) {
       console.error(error)
@@ -35,13 +50,42 @@ const LinkBox: React.FunctionComponent<Props> = ( {url} ) => {
 
 
   return (
-      <Box>
-        <Image src={ogpData.imageUrl} alt={ogpData.title} />
+    <Box borderRadius="lg" borderWidth="1px" px={3} py={6}>
+      <Flex width="100%">
         <Box>
-          <Heading>{ogpData.title}</Heading>
-          <Text>{ogpData.description}</Text>
+          <Link isExternal href={url}>
+          <Image src={ogpData.imageUrl} alt={ogpData.title} display="block"/>
+          </Link>
         </Box>
-      </Box>
+        <VStack width="auto">
+          <Link isExternal href={url}>
+            <Box as="h4" lineHeight="tight" fontWeight="SemiBold" mt={1} isTruncated>{ogpData.title}</Box>
+          </Link>
+          <Text fontSize="sm" fontWeight="light" dangerouslySetInnerHTML={{__html:ogpData.description}} />
+        </VStack>
+      </Flex>
+      <Flex width="100%">
+        <Box>
+          <Link isExternal href={url}>
+            <Text fontSize="xs" fontWeight="SemiBold" as="span"><Image src={faviconData.icons?.src} alt="favicon"/>{ogpData.siteName}</Text>
+          </Link>
+        </Box>
+        <Spacer />
+        <Box  align="flex-end">
+          <Link isExternal href={url}>
+            <Button
+              icon={<ExternalLinkIcon />}
+              label="Shopで詳細を見る"
+              colorScheme="blue"
+              mt={2}
+              variant='solid'
+              float="right">
+                詳細を確認する
+            </Button>
+          </Link>
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
