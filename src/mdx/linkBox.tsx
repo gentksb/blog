1bf01af431c6ca4f6ad3c1a763130e850ff862bb
gensobunya <gen@gensobunya.net>
2021-01-23 22:57:25 +0900

    responsive!

--- src/mdx/linkBox.tsx
+++ src/mdx/linkBox.tsx
@@ -45,41 +45,40 @@ const LinkBox: React.FunctionComponent<Props> = ( {url} ) => {
 
 
   return (
-    <Box borderRadius="lg" borderWidth="1px" px={3} py={6}>
-      <Flex width="100%">
-        <Box maxWidth="20%">
+    <Box p={4} display={{ md: "flex" }} borderWidth="1px" borderRadius="xl">
+      <Box flexShrink={0}>
+        <Image
+          borderRadius="lg"
+          width={{ md: 40 }}
+          src={ogpData.imageUrl}
+          alt={ogpData.title}
+          maxHeight="20vh"
+        />
+      </Box>
+      <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+        <Text
+          fontWeight="bold"
+          textTransform="uppercase"
+          fontSize="sm"
+          letterSpacing="wide"
+          color="teal.600"
+        >
           <Link isExternal href={url}>
-          <Image src={ogpData.imageUrl} alt={ogpData.title} display="block" />
-          </Link>
-        </Box>
-        <VStack width="auto">
-          <Link isExternal href={url}>
-            <Box as="h4" lineHeight="tight" fontWeight="SemiBold" mt={1} overflow="hidden">{ogpData.title}</Box>
-          </Link>
-          <Text fontSize="sm" fontWeight="light" dangerouslySetInnerHTML={{__html:ogpData.description}} />
-        </VStack>
-      </Flex>
-      <Flex width="100%">
-        <Box>
-          <Link isExternal href={url}>
-            <Text fontSize="xs" fontWeight="SemiBold" display="inline-flex"><Image src={ogpData.ogpIcon} alt="favicon" />{ogpData.siteName}</Text>
-          </Link>
-        </Box>
-        <Spacer />
-        <Box  align="flex-end">
-          <Link isExternal href={url}>
-            <Button
-              icon={<ExternalLinkIcon />}
-              label="Shopで詳細を見る"
-              colorScheme="blue"
-              mt={2}
-              variant='solid'
-              float="right">
-                詳細を確認する
-            </Button>
-          </Link>
-        </Box>
-      </Flex>
+                <Text fontSize="xs" fontWeight="SemiBold" display="inline-flex"><Image src={ogpData.ogpIcon} alt="favicon" />{ogpData.siteName}</Text>
+              </Link>
+        </Text>
+        <Link
+          mt={1}
+          display="block"
+          fontSize="lg"
+          lineHeight="normal"
+          fontWeight="semibold"
+          href={url}
+        >
+          {ogpData.title}
+        </Link>
+        <Text mt={2} color="gray.500" dangerouslySetInnerHTML={{__html:ogpData.description}} />
+      </Box>
     </Box>
   )
 }
