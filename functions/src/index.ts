bb97cd904ad992575c2ae60c09b2dbd099ee6db0
gensobunya <gen@gensobunya.net>
2021-01-27 23:05:34 +0900

    呼び出し変数へ変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,11 +3,14 @@ import * as functions from "firebase-functions"
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true })
-  functions.logger.info(
-    process.env.AMAZON_PAAPI_KEY,
-    process.env.AMAZON_PAAPI_SECRET
-  )
-  response.send("Hello from Firebase!")
-})
+
+export const helloWorld = functions
+  .region("asia-northeast1")
+  .https.onCall((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true })
+    functions.logger.info(
+      process.env.AMAZON_PAAPI_KEY,
+      process.env.AMAZON_PAAPI_SECRET
+    )
+    return "Hello from Firebase!"
+  })
