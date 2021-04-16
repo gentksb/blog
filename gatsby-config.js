b9d7c5c701d2caea6207dad10d31f1053e91fdad
gensobunya <gen@gensobunya.net>
2021-04-16 20:11:08 +0900

    restore twitter plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typegen`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-twitter`,
     `gatsby-plugin-emotion`,
     `@chakra-ui/gatsby-plugin`,
     `gatsby-plugin-typescript`,
