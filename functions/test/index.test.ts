44e67ba8642495ba43723c0198c1909d3127545f
gensobunya <gen@gensobunya.net>
2022-02-11 19:27:29 +0900

    一旦保存

--- functions/test/index.test.ts
+++ functions/test/index.test.ts
@@ -1,39 +1,25 @@
 import { expect } from "chai"
 import "mocha"
-
-describe("This", () => {
-  describe("should", () => {
-    it("always pass", () => {
-      expect(true).to.equal(true)
-    })
-  })
-})
-
 import test from "firebase-functions-test"
 const firebaseTest = test()
-
-
-import { getOgpLinkData } from "../src/index"
 import {
   a8LinkData,
   a8LinkDataExpectedResponse,
   amazonLinkData,
   amazonLinkDataExpectedResponse,
-  // invalidAmazonAsinData,
-  // invalidAmazonAsinDataResponse,
   normalLinkData,
   normalLinkDataExpectedResponse
 } from "./testData"
+firebaseTest.mockConfig({
+  amazon: {
+    partner_tag: process.env.PARTNER_TAG,
+    paapi_secret: process.env.AMAZON_PAAPI_SECRET,
+    paapi_key: process.env.AMAZON_PAAPI_KEY
+  }
+})
+import { getOgpLinkData } from "../src/index"
 const wrapped = firebaseTest.wrap(getOgpLinkData)
-before(() =>
-  firebaseTest.mockConfig({
-    amazon: {
-      partner_tag: process.env.PARTNER_TAG,
-      paapi_secret: process.env.AMAZON_PAAPI_SECRET,
-      paapi_key: process.env.AMAZON_PAAPI_KEY
-    }
-  })
-)
+
 after(() => firebaseTest.cleanup())
 
 describe("リンク先OGPデータを取得する", () => {
@@ -45,10 +31,6 @@ describe("リンク先OGPデータを取得する", () => {
     const res = await wrapped(amazonLinkData)
     expect(res).deep.equal(amazonLinkDataExpectedResponse)
   })
-  // it("Amazonリンク不備", async () => {
-  //   const res = await wrapped(invalidAmazonAsinData)
-  //   expect(res).to.throw()
-  // })
   it("A8リンクOGP", async () => {
     const res = await wrapped(a8LinkData)
     expect(res).deep.equal(a8LinkDataExpectedResponse)
