6ae9e2cad62c897960b4b7c6968e5ae24a501996
gentksb <gen@gensobunya.net>
2022-09-19 21:14:44 +0900

    外部リンクは新タブ

--- src/mdx/atoms/link.tsx
+++ src/mdx/atoms/link.tsx
@@ -6,8 +6,16 @@ interface Props {
   href: string
 }
 
-export const MdxLink: React.FC<Props> = ({ children, href }) => (
-  <Link color="teal.500" href={href} _hover={{ textDecoration: "underline" }}>
-    {children}
-  </Link>
-)
+export const MdxLink: React.FC<Props> = ({ children, href }) => {
+  const isExternal = !href.startsWith("https://blog.gensobunya.net/")
+  return (
+    <Link
+      color="teal.500"
+      href={href}
+      _hover={{ textDecoration: "underline" }}
+      isExternal={isExternal}
+    >
+      {children}
+    </Link>
+  )
+}
