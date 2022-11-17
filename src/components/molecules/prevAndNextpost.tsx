cfe266c918502dc9ac810a0e9b4f22e3756ac224
gentksb <gen@gensobunya.net>
2022-11-17 22:43:34 +0900

    喰えり改善

--- src/components/molecules/prevAndNextpost.tsx
+++ src/components/molecules/prevAndNextpost.tsx
@@ -14,34 +14,36 @@ const PrevAndNextPost: React.FunctionComponent<Props> = ({
 }) => {
   const hasPreviousPost = !!previous
   const hasNextPost = !!next
-  const previousPostPath = previous?.fields.slug
-  const nextPostPath = next?.fields.slug
-  const previousPageTitle = previous?.frontmatter.title
-  const nextPageTitle = next?.frontmatter.title
+  const previousPostPath = previous?.fields?.slug
+  const nextPostPath = next?.fields?.slug
+  const previousPageTitle = previous?.frontmatter?.title
+  const nextPageTitle = next?.frontmatter?.title
+
+  //graphqlで取得するnextは過去の記事(index+1)を指している
 
   return (
     <Grid templateColumns="repeat(auto-fill, 50%)" w="100%">
       <GridItem colSpan={[2, 2, 1, 1]} margin={1}>
-        <Link to={previousPostPath} rel="prev">
+        <Link to={nextPostPath} rel="prev">
           <Button
             variant="outline"
-            isDisabled={!hasPreviousPost}
+            isDisabled={!hasNextPost}
             leftIcon={<ArrowBackIcon />}
             width="full"
           >
-            <Text noOfLines={1}>{previousPageTitle}</Text>
+            <Text noOfLines={1}>{nextPageTitle}</Text>
           </Button>
         </Link>
       </GridItem>
       <GridItem colSpan={[2, 2, 1, 1]} margin={1} textAlign="right">
-        <Link to={nextPostPath} rel="next">
+        <Link to={previousPostPath} rel="next">
           <Button
             variant="outline"
-            isDisabled={!hasNextPost}
+            isDisabled={!hasPreviousPost}
             rightIcon={<ArrowForwardIcon />}
             width="full"
           >
-            <Text noOfLines={1}>{nextPageTitle}</Text>
+            <Text noOfLines={1}>{previousPageTitle}</Text>
           </Button>
         </Link>
       </GridItem>
