db68b9756cf4319537dc45551c5b01a3fdf6f468
Gen Takashiba <gen@gensobunya.net>
2022-11-17 12:23:06 +0900

    生成する画像数を減らす (#234)
    * Update gatsby-config.ts
    
    * Update gatsby-config.ts
    
    * Update gatsby-config.ts
    
    * Update gatsby-config.ts
    
    * Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,7 @@
 import type { GatsbyConfig } from "gatsby"
 
+const breakpoints = [375, 768, 1280] //画像生成数を削減するためにサイズを限定
+
 const config: GatsbyConfig = {
   graphqlTypegen: true,
   siteMetadata: {
@@ -27,17 +29,7 @@ const config: GatsbyConfig = {
       }
     },
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {
-          transformOptions: {
-            fit: "inside",
-            cropFocus: "attention"
-          }
-        }
-      }
-    },
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
@@ -49,7 +41,7 @@ const config: GatsbyConfig = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              srcSetBreakpoints: [375, 768, 1280],
+              srcSetBreakpoints: breakpoints,
               maxWidth: 1280, //discover用
               quality: 80,
               withWebp: true,
