4b9c898d7540d9555c1bdf17c9c2ac4ebb1cbd6c
gentksb <gen@gensobunya.net>
2023-08-27 08:15:29 +0900

    aws4fetchで実装しなおし、レスポンスの文字化けをなんとかする

--- cf-astro/functions/api/getOgpFromAsin/[getOgpFromAsin].ts
+++ cf-astro/functions/api/getOgpFromAsin/[getOgpFromAsin].ts
@@ -1,5 +1,5 @@
-import { signRequestForPaapiv5 } from "./awsSigv4"
 import { AmazonItemsRequestParameters } from "amazon-paapi"
+import { AwsClient } from "aws4fetch"
 interface ENV {
   PAAPI_ACCESSKEY: string
   PAAPI_SECRETKEY: string
@@ -19,36 +19,6 @@ export const onRequest: PagesFunction<ENV> = async (context) => {
     const asin = context.params.getOgpFromAsin
     console.log(`Start function with ASIN:${asin}`)
 
-    const body: AmazonRequestBody = {
-      ItemIds: [asin],
-      Resources: [
-        "Images.Primary.Medium",
-        "Images.Primary.Large",
-        "ItemInfo.Features",
-        "ItemInfo.Title"
-      ],
-      Condition: "New",
-      ItemIdType: "ASIN",
-      PartnerTag: context.env.PARTNER_TAG,
-      PartnerType: "Associates",
-      Marketplace: "www.amazon.co.jp"
-    }
-    const reqPropaties = {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/javascript",
-        "Accepet-Language": "en-US",
-        "Content-Type": "application/json; charset=utf-8",
-        "X-Amz-Target": "com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetItems",
-        "Content-Encoding": "amz-1.0"
-      },
-      body: JSON.stringify(body)
-    }
-    const unsignedRequest = new Request(
-      "https://webservices.amazon.co.jp/paapi5/getitems",
-      reqPropaties
-    )
-
     // check context.env for PAAPI_ACCESSKEY, PAAPI_SECRETKEY, PARTNER_TAG
     if (
       typeof context.env.PAAPI_ACCESSKEY !== "string" ||
@@ -59,42 +29,54 @@ export const onRequest: PagesFunction<ENV> = async (context) => {
         status: 500
       })
     } else {
-      const signedRequest = await signRequestForPaapiv5(unsignedRequest, {
-        awsRegion: "us-west-2",
-        awsService: "ProductAdvertisingAPI",
-        awsAccessKeyId: context.env.PAAPI_ACCESSKEY,
-        awsSecretAccessKey: context.env.PAAPI_SECRETKEY
+      const paapiClient = new AwsClient({
+        accessKeyId: context.env.PAAPI_ACCESSKEY,
+        secretAccessKey: context.env.PAAPI_SECRETKEY,
+        service: "ProductAdvertisingAPI",
+        region: "us-west-2"
       })
-      const refreshedRequest = new Request(signedRequest.url, {
-        method: signedRequest.method,
-        headers: signedRequest.headers,
-        body: JSON.stringify(reqPropaties)
-      })
-      console.log(
-        "Request Details: \n",
-        refreshedRequest.url,
-        refreshedRequest.method,
-        JSON.stringify(reqPropaties)
+
+      const body: AmazonRequestBody = {
+        ItemIds: [asin],
+        Resources: [
+          "Images.Primary.Medium",
+          "Images.Primary.Large",
+          "ItemInfo.Features",
+          "ItemInfo.Title"
+        ],
+        Condition: "New",
+        ItemIdType: "ASIN",
+        PartnerTag: context.env.PARTNER_TAG,
+        PartnerType: "Associates",
+        Marketplace: "www.amazon.co.jp"
+      }
+      const reqPropaties = {
+        method: "POST",
+        headers: {
+          Accept: "application/json, text/javascript",
+          "Accepet-Language": "en-US",
+          "Content-Type": "application/json; charset=utf-8",
+          "X-Amz-Target":
+            "com.amazon.paapi5.v1.ProductAdvertisingAPIv1.GetItems",
+          "Content-Encoding": "amz-1.0"
+        },
+        body: JSON.stringify(body)
+      }
+
+      const response = await paapiClient.fetch(
+        "https://webservices.amazon.co.jp/paapi5/getitems",
+        {
+          method: reqPropaties.method,
+          headers: reqPropaties.headers,
+          body: reqPropaties.body
+        }
       )
-      refreshedRequest.headers.forEach((value, key) => {
+      console.log(`Response status: ${response.status}`)
+      response.headers.forEach((value, key) => {
         console.log(`${key}: ${value}`)
       })
 
-      const amazonPaapiResponse = await fetch(refreshedRequest)
-      const amazonPaapiResponseData = await amazonPaapiResponse.text()
-      console.log(
-        "Response Details: \n",
-        amazonPaapiResponse.ok,
-        "\n",
-        amazonPaapiResponse.status,
-        amazonPaapiResponse.statusText,
-        "\n",
-        amazonPaapiResponseData
-      )
-      amazonPaapiResponse.headers.forEach((value, key) => {
-        console.log(`${key}: ${value}`)
-      })
-      return new Response(amazonPaapiResponseData)
+      return new Response(await response.arrayBuffer())
     }
   }
 }
