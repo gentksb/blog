e1b2bcebecbcd185be884127d9025c3a0f25ebe8
gentksb <gen@gensobunya.net>
2023-09-09 14:56:54 +0900

    update setting

--- cf-astro/astro.config.mjs
+++ cf-astro/astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: cloudflare()
-});
+  adapter: cloudflare(),
+  vite: {
+    build: {
+      minify: false
+    }
+  }
+})
