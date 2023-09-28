17ef1d4aeb80a35390025ea453ee76179a524f89
gentksb <gen@gensobunya.net>
2023-09-28 20:37:38 +0900

    always import導入

--- cf-astro/astro.config.ts
+++ cf-astro/astro.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import AutoImport from "astro-auto-import"
+import tailwind from "@astrojs/tailwind"
+import { componentIsHTMLElement } from "astro/runtime/server/render/dom.js"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+  integrations: [
+    AutoImport({
+      imports: ["./src/components/mdx/Test.astro"]
+    }),
+    mdx(),
+    sitemap(),
+    tailwind()
+  ]
+})
