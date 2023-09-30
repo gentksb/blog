d291040811fb906998729acd08e1d078bfd2991b
gentksb <gen@gensobunya.net>
2023-09-30 16:14:53 +0900

    記事レイアウト完了

--- cf-astro/tailwind.config.cjs
+++ cf-astro/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true
+    },
     extend: {}
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")]
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["light"]
+  }
 }
