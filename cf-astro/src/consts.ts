2ef0f8a58acf44da28a3cc84f3841b59a6b649c1
gentksb <gen@gensobunya.net>
2023-09-30 17:52:30 +0900

    記事ページを生成

--- cf-astro/src/consts.ts
+++ cf-astro/src/consts.ts
@@ -1,5 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_TITLE = "幻想サイクル"
+export const SITE_DESCRIPTION =
+  "AJOCC C1レーサーによるロード・MTB・CXの機材運用やレビュー、時々レースレポートを書くブログです"
+export const PROFILE = {
+  social: {
+    twitter: "gen_sobunya",
+    github: "gentksb",
+    instagram: "gen_sobunya",
+    threads: "gen_sobunya"
+  }
+}
--- cf-astro/src/content/config.ts
+++ cf-astro/src/content/config.ts
@@ -1,32 +1,14 @@
 import { defineCollection, z } from "astro:content"
 
-const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-    heroImage: z.string().optional()
-  })
-})
-
 const post = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
       date: z.date().transform((val) => new Date(val)),
-      draft: z.boolean().optional(),
+      draft: z.boolean().optional(), // 歴史的経緯で必須ではない
       tags: z.string().array().min(1),
       cover: image()
     })
 })
 
-export const collections = { blog, post }
+export const collections = { post }
