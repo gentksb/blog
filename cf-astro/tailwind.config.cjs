1914ef5b2e626302e6e3e504d53a22e8b5a9daeb
gentksb <gen@gensobunya.net>
2023-09-30 16:46:00 +0900

    デスクトップのコンテナ動作を調整

--- cf-astro/tailwind.config.cjs
+++ cf-astro/tailwind.config.cjs
@@ -2,9 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    container: {
-      center: true
-    },
     extend: {}
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
