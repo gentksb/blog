3a9f23f4365b1ea4519db0b31cbdc60bb7ff14da
Gen Takashiba <gen@gensobunya.net>
2023-10-05 15:40:51 +0900

    Update rss.xml.js

--- cf-astro/src/pages/rss.xml.js
+++ cf-astro/src/pages/rss.xml.js
@@ -2,6 +2,8 @@ import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts"
 
+// TODO: 今のRSS配信内容を参照して中身を揃える
+
 export async function get(context) {
   const posts = await getCollection("post")
   return rss({
