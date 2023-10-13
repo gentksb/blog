a60ee7181408dc0318603270aaeb0e7d4de91977
gentksb <gen@gensobunya.net>
2023-10-13 22:41:53 +0900

    test

--- cf-astro/src/pages/post/[slug]/ogp.png.ts
+++ cf-astro/src/pages/post/[slug]/ogp.png.ts
@@ -1,21 +1,21 @@
-import { getCollection, getEntry } from "astro:content"
-import type { APIRoute, GetStaticPaths } from "astro"
-import { makeBlogPostOgpImageResponse } from "../../../lib/makeOgpImageResponse"
+// import { getCollection, getEntry } from "astro:content"
+// import type { APIRoute, GetStaticPaths } from "astro"
+// import { makeBlogPostOgpImageResponse } from "../../../lib/makeOgpImageResponse"
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const posts = await getCollection("post")
-  return posts.map((post) => ({ params: { slug: post.slug } }))
-}
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const posts = await getCollection("post")
+//   return posts.map((post) => ({ params: { slug: post.slug } }))
+// }
 
-export const GET: APIRoute = async ({ params }) => {
-  if (!params.slug) {
-    throw new Error("No slug provided")
-  } else {
-    const post = await getEntry("post", params.slug)
+// export const GET: APIRoute = async ({ params }) => {
+//   if (!params.slug) {
+//     throw new Error("No slug provided")
+//   } else {
+//     const post = await getEntry("post", params.slug)
 
-    return await makeBlogPostOgpImageResponse({
-      title: post?.data.title ?? "no title",
-      image: post?.data.cover
-    })
-  }
-}
+//     return await makeBlogPostOgpImageResponse({
+//       title: post?.data.title ?? "no title",
+//       image: post?.data.cover
+//     })
+//   }
+// }
