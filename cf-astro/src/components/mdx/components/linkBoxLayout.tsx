7ac94ed2b9a20edfe529ea009797d000ccb056a2
gentksb <gen@gensobunya.net>
2023-10-13 22:33:38 +0900

    LinkBox完了

--- cf-astro/src/components/mdx/components/linkBoxLayout.tsx
+++ cf-astro/src/components/mdx/components/linkBoxLayout.tsx
@@ -6,17 +6,20 @@ export const LinkBoxLayout: React.FunctionComponent<OgpData> = ({
   pageurl,
   ogpImageUrl
 }) => (
-  <a href={pageurl} target="_blank" rel="noopener noreferrer">
-    <div className="not-prose card card-side rounded-none border border-base-300 bg-base-100">
+  <a
+    href={pageurl}
+    className="link-hover link"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <div className="not-prose card card-side mx-2 rounded-none border border-base-300 bg-base-100">
       <div className="card-body p-2">
-        <span className="link card-title text-base leading-none">
-          {ogpTitle}
-        </span>
-        <div className="card-subtitle text-gray line-clamp-2 text-xs">
+        <span className="card-title text-base leading-none">{ogpTitle}</span>
+        <div className="card-subtitle text-gray line-clamp-2 text-xs font-normal">
           {ogpDescription}
         </div>
       </div>
-      <div className="shrink">
+      <div className="max-w-[30%] shrink">
         <figure className="h-full w-full object-cover">
           <img src={ogpImageUrl ?? ""} className="h-full" />
         </figure>
