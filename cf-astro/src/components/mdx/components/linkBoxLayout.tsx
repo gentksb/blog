254230fce0da465ca3247c99a16b55f46032135c
gentksb <gen@gensobunya.net>
2023-10-14 20:48:00 +0900

    commit

--- cf-astro/src/components/mdx/components/linkBoxLayout.tsx
+++ cf-astro/src/components/mdx/components/linkBoxLayout.tsx
@@ -7,33 +7,36 @@ export const LinkBoxLayout: React.FunctionComponent<OgpData> = ({
   pageurl,
   ogpImageUrl
 }) => {
-  const isExternal = !(pageurl?.startsWith("https://blog.gensobunya")) ?? true
+  const isExternal = !pageurl?.startsWith("https://blog.gensobunya") ?? true
   const hostname = pageurl ? new URL(pageurl).hostname : ""
 
   return (
-  <a
-    href={pageurl}
-    className="link-hover link"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <div className="not-prose card card-side rounded-none border border-base-300 bg-base-100">
-      <div className="card-body p-2">
-        <div className="card-title text-sm md:text-base leading-none text-md text-secondary-focus line-clamp-2">{isExternal? <MdOpenInNew className="inline" /> : ""}{ogpTitle}</div>
-        <div className="card-body p-0 text-secondary line-clamp-2 max-h-[3em] text-xs md:text-sm font-normal">
-          {ogpDescription}
+    <a
+      href={pageurl}
+      className="link-hover link"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="not-prose card card-side mx-2 rounded-none border border-base-300 bg-base-100">
+        <div className="card-body p-2">
+          <div className="text-md card-title line-clamp-2 text-sm leading-none text-secondary-focus md:text-base">
+            {isExternal ? <MdOpenInNew className="inline" /> : ""}
+            {ogpTitle}
+          </div>
+          <div className="card-body line-clamp-2 max-h-[3em] p-0 text-xs font-normal text-secondary md:text-sm">
+            {ogpDescription}
+          </div>
+          <div className="card-subtitle text-xs text-secondary md:text-sm">
+            <MdWeb className="inline" />
+            {hostname}
+          </div>
         </div>
-        <div className="card-subtitle text-xs md:text-sm text-secondary">
-          <MdWeb className="inline" />
-          {hostname}
+        <div className="max-h-28 max-w-[30%] shrink md:max-h-36">
+          <figure className="h-full w-full object-cover">
+            <img src={ogpImageUrl ?? ""} className="h-full" />
+          </figure>
         </div>
       </div>
-      <div className="max-h-28 md:max-h-36 max-w-[30%] shrink">
-        <figure className="h-full w-full object-cover">
-          <img src={ogpImageUrl ?? ""} className="h-full" />
-        </figure>
-      </div>
-    </div>
-  </a>
-)
+    </a>
+  )
 }
