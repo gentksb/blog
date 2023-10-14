10a842f3bed1e54d0dd28ade5c2579ca9544f751
gentksb <gen@gensobunya.net>
2023-10-14 18:33:11 +0900

    LinkBoxよし

--- cf-astro/src/components/mdx/components/linkBoxLayout.tsx
+++ cf-astro/src/components/mdx/components/linkBoxLayout.tsx
@@ -1,11 +1,16 @@
 import type { OgpData } from "../../../../@types/ogpData-type"
+import { MdOpenInNew, MdWeb } from "react-icons/md"
 
 export const LinkBoxLayout: React.FunctionComponent<OgpData> = ({
   ogpTitle,
   ogpDescription,
   pageurl,
   ogpImageUrl
-}) => (
+}) => {
+  const isExternal = !(pageurl?.startsWith("https://blog.gensobunya")) ?? true
+  const hostname = pageurl ? new URL(pageurl).hostname : ""
+
+  return (
   <a
     href={pageurl}
     className="link-hover link"
@@ -14,12 +19,16 @@ export const LinkBoxLayout: React.FunctionComponent<OgpData> = ({
   >
     <div className="not-prose card card-side mx-2 rounded-none border border-base-300 bg-base-100">
       <div className="card-body p-2">
-        <span className="card-title text-base leading-none">{ogpTitle}</span>
-        <div className="card-subtitle text-gray line-clamp-2 text-xs font-normal">
+        <div className="card-title text-sm md:text-base leading-none text-md text-secondary-focus line-clamp-2">{isExternal? <MdOpenInNew className="inline" /> : ""}{ogpTitle}</div>
+        <div className="card-body p-0 text-secondary line-clamp-2 max-h-[3em] text-xs md:text-sm font-normal">
           {ogpDescription}
         </div>
+        <div className="card-subtitle text-xs md:text-sm text-secondary">
+          <MdWeb className="inline" />
+          {hostname}
+        </div>
       </div>
-      <div className="max-h-24 max-w-[30%] shrink">
+      <div className="max-h-28 md:max-h-36 max-w-[30%] shrink">
         <figure className="h-full w-full object-cover">
           <img src={ogpImageUrl ?? ""} className="h-full" />
         </figure>
@@ -27,3 +36,4 @@ export const LinkBoxLayout: React.FunctionComponent<OgpData> = ({
     </div>
   </a>
 )
+}
