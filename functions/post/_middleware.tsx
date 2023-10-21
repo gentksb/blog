0e6342306902914f219420a040cff031d0dbd9dd
gentksb <gen@gensobunya.net>
2023-10-21 16:13:54 +0900

    動くもの

--- functions/post/_middleware.tsx
+++ functions/post/_middleware.tsx
@@ -3,16 +3,27 @@ import vercelOGPagesPlugin from "@cloudflare/pages-plugin-vercel-og"
 
 interface Props {
   ogTitle: string
+  ogImageSrc: string
 }
 
 export const onRequest = vercelOGPagesPlugin<Props>({
   imagePathSuffix: "/twitter-og.png",
-  component: ({ ogTitle, pathname }) => {
+  component: ({ ogTitle, ogImageSrc }) => {
+    // テキストが真ん中にそろわないので、要調整
+    // わかりやすくテキストにバックグラウンドカラーを設定
     return (
-      <div style={{ display: "flex" }}>
-        {ogTitle}
-        <br />
-        {pathname}
+      <div tw="flex flex-col w-full h-full">
+        <div tw="flex w-full h-full">
+          <img src={ogImageSrc} tw="w-full h-full object-cover" />
+        </div>
+        <div tw="absolute top-0 left-0 bg-black/60 w-full h-full "></div>
+        <div tw="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-4 text-4xl text-center bg-gray-100">
+          {ogTitle}
+        </div>
+        <div tw="flex text-gray-100 text-2xl justify-end absolute bottom-0 right-0">
+          <img src="https://blog.gensobunya.net/image/logo.jpg" tw="h-[2rem]" />
+          幻想サイクル
+        </div>
       </div>
     )
   },
@@ -21,6 +32,11 @@ export const onRequest = vercelOGPagesPlugin<Props>({
       'meta[property="og:title"]': (props) => ({
         element(element) {
           props.ogTitle = element.getAttribute("content")
+        }
+      }),
+      'meta[property="og:image"]': (props) => ({
+        element(element) {
+          props.ogImageSrc = element.getAttribute("content")
         }
       })
     }
