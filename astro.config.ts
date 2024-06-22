a12884498b24565a22e419b12200820bdc69996d
gentksb <gen@gensobunya.net>
2024-06-22 17:40:15 +0900

    fix wasm module resolution

--- astro.config.ts
+++ astro.config.ts
@@ -1,38 +1,52 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import AutoImport from "astro-auto-import";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import AutoImport from "astro-auto-import"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
 // import remarkDescription from "astro-remark-description"
-import partytown from "@astrojs/partytown";
-import icon from "astro-icon";
-import { rehypeExcerptContent } from "./src/plugin/rehypeExcerpt";
+import partytown from "@astrojs/partytown"
+import icon from "astro-icon"
+import { rehypeExcerptContent } from "./src/plugin/rehypeExcerpt"
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.gensobunya.net/",
-  integrations: [AutoImport({
-    imports: ["./src/components/mdx/LinkBox.astro", {
-      "./src/components/mdx/positive.tsx": ["PositiveBox"],
-      "./src/components/mdx/negative.tsx": ["NegativeBox"]
-    }]
-  }), mdx(), sitemap(), tailwind(), react(), icon(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  })],
+  integrations: [
+    AutoImport({
+      imports: [
+        "./src/components/mdx/LinkBox.astro",
+        {
+          "./src/components/mdx/positive.tsx": ["PositiveBox"],
+          "./src/components/mdx/negative.tsx": ["NegativeBox"]
+        }
+      ]
+    }),
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    icon(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })
+  ],
   markdown: {
     rehypePlugins: [rehypeExcerptContent]
   },
   vite: {
     ssr: {
       // to fix react-icons ESM import error
-      noExternal: ["react-icons"]
+      noExternal: ["react-icons", "html-rewriter-wasm"]
+    },
+    optimizeDeps: {
+      exclude: ["html-rewriter-wasm"]
     }
   },
   output: "server",
   adapter: cloudflare()
-});
+})
