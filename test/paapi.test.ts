a12a5d08a09d12b18f1a168b6025389fcb842005
gentksb <gen@gensobunya.net>
2024-06-25 18:34:06 +0900

    テストコマンド修正

--- test/paapi.test.ts
+++ test/paapi.test.ts
@@ -3,11 +3,7 @@ import { testAsin, amazonLinkDataExpectedResponse } from "./testData"
 import { getAmazonProductInfo } from "@lib/getAmazonProductInfo"
 import { env } from "cloudflare:test"
 
-// ローカルテストはCloudflareのenv、GHAは環境変数から取得
-// なぜそうしないといけないのかは不明
-const PAAPI_ACCESSKEY = env.PAAPI_ACCESSKEY ?? process.env.PAAPI_ACCESSKEY
-const PAAPI_SECRETKEY = env.PAAPI_SECRETKEY ?? process.env.PAAPI_SECRETKEY
-const PARTNER_TAG = env.PARTNER_TAG ?? process.env.PARTNER_TAG
+const { PAAPI_ACCESSKEY, PAAPI_SECRETKEY, PARTNER_TAG } = env
 
 if (!PAAPI_ACCESSKEY || !PAAPI_SECRETKEY || !PARTNER_TAG) {
   throw new Error("Environment variables are not valid")
