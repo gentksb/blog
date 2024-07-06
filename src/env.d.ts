7506aaec36d0b60fa0e6b778e12b60ccb03432ea
gentksb <gen@gensobunya.net>
2024-07-06 18:14:34 +0900

    type fix & revert temp code

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,9 +6,5 @@
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>
 
 declare namespace App {
-  interface Locals extends Runtime {
-    otherLocals: {
-      test: string
-    }
-  }
+  interface Locals extends Runtime {}
 }
