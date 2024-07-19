3865daee43e2b3ecb934555a929daa2c970673e7
gentksb <gen@gensobunya.net>
2024-07-19 18:42:54 +0900

    internal image service

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config"
+import { defineConfig, passthroughImageService } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import AutoImport from "astro-auto-import"
@@ -6,31 +6,22 @@ import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
 import partytown from "@astrojs/partytown"
 import icon from "astro-icon"
-import { imageService } from "@unpic/astro/service"
 
-// ローカル開発のために環境変数を設定
-const isCfPages = process.env.CF_PAGES === "1" ? true : false
-const mainImageService = isCfPages ? "imgix" : "astro"
-console.log(
-  `CF_PAGES detect: ${isCfPages}, mainImageService: ${mainImageService}`
-)
+// ローカル開発時は画像サービスをパススルー
+const imageServiceConfig = import.meta.env.DEV
+  ? passthroughImageService()
+  : {
+      entrypoint: "./src/entrypoint/cfImageService",
+      config: {
+        maxWidth: 800
+      }
+    }
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.gensobunya.net/",
   image: {
-    service: imageService({
-      fallbackService: mainImageService,
-      cdnOptions: {
-        imgix: {
-          domain: "gensobunya.imgix.net",
-          defaultParams: {
-            auto: "format,compress",
-            w: "800"
-          }
-        }
-      }
-    })
+    service: imageServiceConfig
   },
   integrations: [
     AutoImport({
---
+++ src/entrypoint/cfImageService.ts
@@ -0,0 +1,63 @@
+import type { AstroConfig, ExternalImageService, ImageTransform } from "astro"
+import { baseService } from "astro/assets"
+
+export interface CloudflareImageServiceConfig {
+  maxWidth?: number
+  quality?: number
+}
+
+const service: ExternalImageService = {
+  ...baseService,
+  validateOptions(options: ImageTransform, imageConfig: AstroConfig["image"]) {
+    const serviceConfig = imageConfig.service.config
+
+    // Enforce the user set max width.
+    if (options.width ?? 0 > serviceConfig.maxWidth) {
+      console.warn(
+        `Image width ${options.width} exceeds max width ${serviceConfig.maxWidth}. Falling back to max width.`
+      )
+      options.width = serviceConfig.maxWidth
+    }
+
+    return options
+  },
+
+  getURL(options: ImageTransform): string {
+    const { src: imageSrc, width, height, format, quality = 80, fit } = options
+
+    // Cloudflare Image Resizingのオプションを構築
+    const cfOptions: string[] = []
+
+    if (width) cfOptions.push(`width=${width}`)
+    if (height) cfOptions.push(`height=${height}`)
+    if (format) cfOptions.push(`format=${format}`)
+    if (quality) cfOptions.push(`quality=${quality}`)
+    if (fit) cfOptions.push(`fit=${fit}`)
+
+    // ソース画像のパスを取得（URLの場合はそのまま使用）
+    // ImageMetaData以外の画像の場合、自サイトドメインのみ許可
+    if (
+      typeof imageSrc === "string" &&
+      !imageSrc.startsWith("https://blog.gensobunya.net/")
+    ) {
+      throw new Error("External image source URL in Astro Image component")
+    }
+    const sourcePath = typeof imageSrc === "string" ? imageSrc : imageSrc.src
+    const baseUrl = import.meta.env.BASE_URL
+      ? `${import.meta.env.BASE_URL}/`
+      : ""
+    // Cloudflare Image ResizingのURL形式に変換
+    const imagePath = `${baseUrl}/cdn-cgi/image/${cfOptions.join(",")}${sourcePath}`
+    // console.log(
+    //   `baseUrl: ${baseUrl}`,
+    //   `sourcePath: ${sourcePath}`,
+    //   `cfOptions: ${cfOptions}`,
+    //   `rawPath: ${imagePath}`
+    // )
+
+    // @cloudflare/internal-helperの実装を参考に
+    return imagePath.startsWith("/") ? imagePath.substring(1) : imagePath
+  }
+}
+
+export default service
