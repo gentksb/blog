408c32c75e4fa899461b315b83cf61ed32ef1cc7
gentksb <gen@gensobunya.net>
2024-07-19 10:12:20 +0900

    unpic test

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, passthroughImageService } from "astro/config"
+import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import AutoImport from "astro-auto-import"
@@ -6,12 +6,23 @@ import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
 import partytown from "@astrojs/partytown"
 import icon from "astro-icon"
+import { imageService } from "@unpic/astro/service"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.gensobunya.net/",
   image: {
-    service: passthroughImageService()
+    service: imageService({
+      cdnOptions: {
+        imgix: {
+          domain: "gensobunya.imgix.net",
+          defaultParams: {
+            auto: "format,compress",
+            w: "800"
+          }
+        }
+      }
+    })
   },
   integrations: [
     AutoImport({
