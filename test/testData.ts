9a055c3e0df7a2ad1978ccd526ed7925255600d3
gentksb <gen@gensobunya.net>
2025-05-26 21:09:34 +0900

    テストをAmazonの商品名に左右されないよう修正

--- test/testData.ts
+++ test/testData.ts
@@ -17,34 +17,3 @@ export const normalLinkDataExpectedResponse: OgpData = {
 // 上記2つのdescription版
 
 export const testAsin = "B004N3APGO"
-
-// A part of AmazonItemsResponse Type
-export const amazonLinkDataExpectedResponse = {
-  ItemsResult: {
-    Items: [
-      {
-        ASIN: testAsin,
-        DetailPageURL:
-          "https://www.amazon.co.jp/dp/B004N3APGO?tag=gensobunya-22&linkCode=ogi&th=1&psc=1",
-        ItemInfo: {
-          Title: {
-            DisplayValue:
-              "Amazonギフトカード(Eメール送信,携帯電話番号にも送信可)-お礼、お祝い(Amazonベーシック)",
-            Label: "Title",
-            Locale: "ja_JP"
-          }
-        },
-        Images: {
-          Primary: {
-            Large: {
-              URL: "https://m.media-amazon.com/images/I/41xROrVX5kL._SL500_.jpg"
-            },
-            Medium: {
-              URL: "https://m.media-amazon.com/images/I/41xROrVX5kL._SL160_.jpg"
-            }
-          }
-        }
-      }
-    ]
-  }
-}
